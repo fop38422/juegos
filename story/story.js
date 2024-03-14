e78f66ccc1a9c5e0b6c58ed17990eff842a06532
angel <fop38422@educastur.es>
2024-03-14 19:53:40 +0100

    storytelling empezao dios me salve de esta

---
+++ story/story.js
@@ -0,0 +1,15 @@
+let primeraOpcion = 0;
+let segundaOpcion = 0;
+let terceraOpcion = 0;
+
+function opcion0(opcion) {
+    let texto = document.getElementById("texto");
+    let b1 = document.getElementById("b1");
+    let b2 = document.getElementById("b2");
+
+    if (opcion == 1) {
+
+    }else{
+
+    }
+}
